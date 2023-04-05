a5b2787b8f33c1edf82401d7484ad5b9356c99ea
Анатолий <Chepanov199287@gmail.com>
2023-04-05 22:54:08 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillFactory_5_3_8
+{
+    class Program
+    {
+        static int[] GetArrayFromConsole(ref int number)
+        {
+            var result = new int[number];
+            Console.WriteLine(result.Length + 1);            
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            int number = 5;
+
+            GetArrayFromConsole(ref number);
+            Console.ReadKey();
+        }
+    }
+}
